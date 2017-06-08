869f4a1be35418b7e8177c4517ace254e70231d8
Zoey Wolfe <zoey.a.wolfe@gmail.com>
2017-06-08 15:26:40 -0400

    initialize

---
+++ texts.js
@@ -0,0 +1,19 @@
+var texts = [
+	'Tom Léger was a minor cult leader active in the second decade of the new millennium.',
+
+	'Julie Blair was a genius who brightened the room she was in, but whose sweet tooth consumed her.',
+
+	'Tyler Beebe was a pile of hair with a great ass.',
+
+	'Natalie Braginsky was a computer whose cooling fans weren\'t working.',
+
+	'Kayla Reed was a google employee that once got kicked out of an electronic music festival for hot naked makeouts with her then best friend, Hayley.',
+
+	'Hayley Anthony was the goddess of incremental systemic improvements, with the good hair.',
+
+	'Robin Hill was the love interest in a now forgotten Indiana Jones holiday special.',
+
+	'Justine was a talented writer who was hard to pin down, unless you were a cute guy.',
+
+	'Colette Arand was a poet that should have been the first transsexual WWE star besides Chyna.',
+]
